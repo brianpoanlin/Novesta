5eeaf5b7092298b198cdd7f6f88247850c352441
abhi-ganta <31359843+abhi-ganta@users.noreply.github.com>
2017-09-24 00:55:14 -0400

    buyandsell

--- iOS/Novesta/Novesta/buy_and_sell_ViewController.swift
+++ iOS/Novesta/Novesta/buy_and_sell_ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class buy_and_sell_ViewController: UIViewController {
-
+    let crypRef = Database.database().reference(withPath: "master_crypto")
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -30,5 +32,52 @@ class buy_and_sell_ViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    func pullData(){
+        Database.database().reference(withPath: "users").child(universalUserID).child("portfolio").queryOrdered(byChild: "portfolio")
+            .observeSingleEvent(of: .value, with: { snapshot in
+                
+                for child in snapshot.children.allObjects as? [DataSnapshot] ?? []{
+                    
+                    let value = child.value as! [String: Any]
+                    
+                    print(value)
+                    
+                    let crypDataSimp: [String: AnyObject] =  ["id":value["id"] as AnyObject, "quantity":value["quantity"] as AnyObject]
+                    self.cryp_id_list.append(crypDataSimp as NSDictionary)
+                    
+                    Database.database().reference(withPath: "name_crypto").child(value["id"] as! String).observeSingleEvent(of: .value) { (snapshot:DataSnapshot) in
+                        if (snapshot.value != nil) {
+                            let val:[String: AnyObject] =  [
+                                "24h_volume_usd":snapshot.childSnapshot(forPath: "24h_volume_usd").value as! String as AnyObject,
+                                "available_supply":snapshot.childSnapshot(forPath: "available_supply").value as! String as AnyObject,
+                                "id":snapshot.childSnapshot(forPath: "id").value as! String as AnyObject,
+                                "last_updated":snapshot.childSnapshot(forPath: "last_updated").value as! String as AnyObject,
+                                "market_cap_usd":snapshot.childSnapshot(forPath: "market_cap_usd").value as! String as AnyObject,
+                                "name":snapshot.childSnapshot(forPath: "name").value as! String as AnyObject,
+                                "percent_change_1h":snapshot.childSnapshot(forPath: "percent_change_1h").value as! String as AnyObject,
+                                "percent_change_24h":snapshot.childSnapshot(forPath: "percent_change_24h").value as! String as AnyObject,
+                                "percent_change_7d":snapshot.childSnapshot(forPath: "percent_change_7d").value as! String as AnyObject,
+                                "price_btc":snapshot.childSnapshot(forPath: "price_btc").value as! String as AnyObject,
+                                "price_usd":snapshot.childSnapshot(forPath: "price_usd").value as! String as AnyObject,
+                                "rank":snapshot.childSnapshot(forPath: "rank").value as! String as AnyObject,
+                                "symbol":snapshot.childSnapshot(forPath: "symbol").value as! String as AnyObject,
+                                "total_supply":snapshot.childSnapshot(forPath: "total_supply").value as! String as AnyObject,
+                                "user_quantity":value["quantity"] as! String as AnyObject]
+                            
+                            self.cryp_loc_list.append(val as NSDictionary)
+                            
+                            self.tableView.reloadData()
+                            
+                        }
+                        else {
+                        }
+                    }
+                    
+                }
+                
+                print("DONE")
+            })
+        
+        
+    }
 }
