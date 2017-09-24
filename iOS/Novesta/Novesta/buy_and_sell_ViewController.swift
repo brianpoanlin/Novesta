7b0472851d4273c0a5964052224286a9ea7f9ce9
abhi-ganta <31359843+abhi-ganta@users.noreply.github.com>
2017-09-24 00:43:05 -0400

    buy_sell

---
+++ iOS/Novesta/Novesta/buy_and_sell_ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  buy_and_sell_ViewController.swift
+//  Novesta
+//
+//  Created by Abhitej R Ganta on 9/24/17.
+//  Copyright © 2017 Brian Lin. All rights reserved.
+//
+
+import UIKit
+
+class buy_and_sell_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
